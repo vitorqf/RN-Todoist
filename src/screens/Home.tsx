8b93fc2a960cc5e3540ab7115cfd429d37777580
Vitor Rafael <rqfvitor@gmail.com>
2023-10-20 13:57:54 -0300

    feat: add header text badges

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, TextProps, View } from "react-native";
 import { Input } from "../components/Input";
 import styled from "styled-components";
 import Logo from "../assets/Logo.svg"
 import { Button } from "../components/Button";
+
+type Variants = 'blue' | 'purple'
+
 
 const Container = styled(View)`
     flex: 1;
@@ -43,6 +46,31 @@ const Header = styled(View)`
     justify-content: space-between;
     align-items: center;
 `
+
+const HeaderTextContainer = styled(View)`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 8px;
+`;
+
+const CountBadge = styled(View)`
+    ${({ theme }) => theme.font.sizes.sm};
+    color: ${({ theme }) => theme.colors.neutral.gray[100]};
+    background-color: ${({ theme }) => theme.colors.neutral.gray[400]};
+    border-radius: 50px;
+    padding: 0 8px;
+    
+    align-items: center;
+    justify-content: center;
+`;
+
+const HeaderText = styled(Text)<{ variant: Variants }>`
+    ${({ theme }) => theme.font.sizes.md};
+    color: ${({ variant, theme }) => theme.colors.brand[variant]};
+    font-weight: bold;
+
+`;
 export default function Home() {
     return (
         <Container>
@@ -57,8 +85,19 @@ export default function Home() {
 
                 <List>
                     <Header>
-                        <Text style={{color: "#fff"}}>Criadas</Text>
-                        <Text style={{color: "#fff"}}>Concluídas</Text>
+                        <HeaderTextContainer>
+                            <HeaderText variant="blue">Criadas</HeaderText>
+                            <CountBadge>
+                                <Text style={{ color: '#fff'}}>0</Text>
+                            </CountBadge>
+                        </HeaderTextContainer>
+
+                        <HeaderTextContainer>
+                            <HeaderText variant="purple">Concluídas</HeaderText>
+                            <CountBadge>
+                                <Text style={{ color: '#fff'}}>0</Text>
+                            </CountBadge>
+                        </HeaderTextContainer>
                     </Header>
                 </List>
             </GrayBackground>

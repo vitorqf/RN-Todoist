9a97afaf1c3f605abfa44be43f31652377770ec7
rqfvitor@gmail.com <rqfvitor@gmail.com>
2023-10-20 10:40:58 -0300

    feat: add web-like svg handling

--- App.tsx
+++ App.tsx
@@ -1,17 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/styles/theme'
-import { Input } from './src/components/Input';
+import Home from './src/screens/Home';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-        <View style={{flex: 1, paddingTop: 60, marginLeft: 20}}>
-          <StatusBar style="auto" />
-          <Input placeholder='Adicione uma nova tarefa' />
-          <Text>Hello World</Text>
-        </View>
+          <StatusBar translucent style='light' />
+          <Home />
+
     </ThemeProvider>
   );
 }

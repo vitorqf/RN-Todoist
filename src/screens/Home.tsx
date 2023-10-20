4dd0e80379e8641b3663b54fbfd9bfea94c24ca6
rqfvitor@gmail.com <rqfvitor@gmail.com>
2023-10-20 11:30:49 -0300

    feat: add list header

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { Input } from "../components/Input";
 import styled from "styled-components";
 import Logo from "../assets/Logo.svg"
@@ -18,11 +18,12 @@ const BlackBackground = styled(View)`
 const GrayBackground = styled(View)`
     flex: 7;
     background-color: ${({ theme }) => theme.colors.neutral.gray[600]};
+    padding: 0 24px;
+    align-items: center;
 `
 
 const Form = styled(View)`
    position: absolute;
-   padding: 0 24px;
    max-width: 100%;
    top: -28px;
 
@@ -30,6 +31,18 @@ const Form = styled(View)`
    gap: 4px;
 `
 
+const List = styled(View)`
+    width: 100%;
+    padding: 48px 0 16px;
+    border-bottom-width: 1px; 
+    border-color: ${({ theme }) => theme.colors.neutral.gray[400]};
+`
+
+const Header = styled(View)`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
 export default function Home() {
     return (
         <Container>
@@ -38,9 +51,16 @@ export default function Home() {
             </BlackBackground>
             <GrayBackground>
                 <Form>
-                    <Input placeholder="Adicione uma tarefa" />
+                    <Input placeholder="Adicione uma nova tarefa" />
                     <Button />
                 </Form>
+
+                <List>
+                    <Header>
+                        <Text style={{color: "#fff"}}>Criadas</Text>
+                        <Text style={{color: "#fff"}}>Concluídas</Text>
+                    </Header>
+                </List>
             </GrayBackground>
         </Container>
     )

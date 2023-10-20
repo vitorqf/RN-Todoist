f6ab774032f18a60d08ae56c6629d6bcd240cc81
rqfvitor@gmail.com <rqfvitor@gmail.com>
2023-10-20 09:38:34 -0300

    feat: add styled components theming

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/styles/theme'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+        <View>
+          <StatusBar style="auto" />
+          <Text>Hello World</Text>
+        </View>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/theme.ts
@@ -0,0 +1,44 @@
+const theme = {
+    colors: {
+        brand: {
+            "purple_dark": "#5E60CE",
+            "purple": "#8284FA",
+            "blue_dark": "#1E6F9F",
+            "blue": "#4EA8DE"
+        },
+        neutral: {
+            gray:  {
+                700: "#0D0D0D",
+                600: "#1A1A1A",
+                500: "#262626",
+                400: "#333333",
+                300: "#808080",
+                200: "#D9D9D9",
+                100: "#F2F2F2"
+            }
+        },
+        red: {
+            feedback: {
+                danger: "#E25858"
+            }
+        }
+    },
+    font: {
+        sizes: {
+            sm: {
+                "font-size": "12px",
+                "line-height": "16px"
+            },
+            md: {
+                "font-size": "14px",
+                "line-height": "20px"
+            },
+            lg: {
+                "font-size": "16px",
+                "line-height": "32px"
+            },
+        }
+    }
+}
+
+export default theme;

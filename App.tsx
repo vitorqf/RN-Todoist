c4a32f1b9d3f118839c30fd8b504e286ddc8f553
rqfvitor@gmail.com <rqfvitor@gmail.com>
2023-10-20 10:11:27 -0300

    feat: add base input component

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/styles/theme'
+import { Input } from './src/components/Input';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-        <View>
+        <View style={{flex: 1, paddingTop: 60, marginLeft: 20}}>
           <StatusBar style="auto" />
+          <Input placeholder='Adicione uma nova tarefa' />
           <Text>Hello World</Text>
         </View>
     </ThemeProvider>
---
+++ src/components/Input.tsx
@@ -0,0 +1,53 @@
+import { Fragment, useState } from "react";
+import { TextInput, TextInputProps } from "react-native";
+import { styled } from "styled-components";
+import theme from "../styles/theme";
+
+interface IInputProps extends TextInputProps { 
+}
+
+const StyledInput = styled(TextInput).attrs(({ theme }) => {
+    return {
+        placeholderTextColor: theme.colors.neutral.gray[300]
+    }
+})`
+    width: 100%;
+    height: 52px;
+    align-items: center;
+
+    border-radius: 8px;
+    padding: 0 16px;
+    border-width: 1px;
+    
+    ${({ theme }) => theme.font.sizes.md};
+
+    background-color: ${({ theme }) => theme.colors.neutral.gray[700]};
+    color: ${({ theme }) => theme.colors.neutral.gray[100]};
+`;
+
+const COLORS = {
+    focused: {
+        borderColor: theme.colors.brand.purple
+    },
+    unfocused: {
+        borderColor: theme.colors.neutral.gray[500]
+    }
+}
+
+export function Input({ ...others }: IInputProps) {
+    const [inputBorderColor, setInputBordercolor] = useState(COLORS.unfocused.borderColor);
+
+    function handleFocus() {
+        setInputBordercolor(COLORS.focused.borderColor);
+    }
+
+    return (
+        <Fragment>
+            <StyledInput {...others} onFocus={handleFocus} 
+                style={{
+                    borderColor: inputBorderColor
+                }}
+            />
+        </Fragment>
+    )
+}

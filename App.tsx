be10694ecd2b17a5710757c05f4379752b9041cb
rqfvitor@gmail.com <rqfvitor@gmail.com>
2023-10-20 21:02:54 -0300

    feat: add fonts, splash screen and app icon

--- App.tsx
+++ App.tsx
@@ -2,12 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
 import Home from './src/screens/Home';
 import theme from './src/styles/theme';
+import {  useFonts, Inter_400Regular, Inter_700Bold, Inter_500Medium } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import { View } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const fontsLoaded = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+    Inter_500Medium
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync();
+      }
+    }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar translucent style="light" />
-      <Home />
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <StatusBar translucent style="light" />
+        <Home />
+      </View>
     </ThemeProvider>
   );
 }
